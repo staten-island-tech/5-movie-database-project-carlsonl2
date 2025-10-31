75da1e3d0fcfd73a9bbdcfe20a1ac8fba917e322
carlsonl2 <carlsonl2@nycstudents.net>
2025-10-31 06:21:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+for index, item in enumerate(data):
+    for i in range(index):
+        print(f"{data[i]['title']}")
